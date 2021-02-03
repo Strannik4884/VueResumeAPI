8b504f6ce99aed04329aa65d4259d879682c4d95
Strannik4884 <strannik4884@mail.ru>
2021-02-03 21:55:25 +0300

    Remove empty files and code formatting

--- config/bundles.php
+++ config/bundles.php
@@ -4,5 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
-    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true]
 ];
