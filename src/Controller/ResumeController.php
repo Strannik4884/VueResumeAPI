01e84e4d1bae76dd0df5df03be11c078a49d1335
Strannik4884 <strannik4884@mail.ru>
2021-02-03 17:53:34 +0300

    Add resume edit REST API point and code formatting

--- src/Controller/ResumeController.php
+++ src/Controller/ResumeController.php
@@ -39,34 +39,73 @@ class ResumeController extends AbstractFOSRestController
     {
         $client = new DB($this->getParameter('app.db_url'));
         $collection = $client->selectCollection($this->getParameter('app.db_name'), $this->getParameter('app.db_collection'));
-        try
-        {
+        try {
             // get item by id
             $item = $collection->findOne(array('_id' => new ObjectId($id)));
             // if item found
-            if(isset($item))
-            {
+            if (isset($item)) {
                 $item['id'] = $item['_id']->__toString();
                 unset($item['_id']);
                 return $this->json($item);
-            }
-            else
-            {
+            } else {
                 return $this->json([
                     'code' => 404,
                     'message' => 'Resume with id ' . $id . ' not found'
-                ],
-                404);
+                ], 404);
             }
-        }
-        catch (\Exception $exception)
-        {
+        } catch (\Exception $exception) {
             return $this->json([
                 'code' => 400,
                 'message' => 'Incorrect resume id'
-            ],
-            400);
+            ], 400);
         }
+    }
+
+    /**
+     * @Rest\Post("/api/cv/{id}/edit", name="edit_cv_by_id")
+     * @param string $id
+     * @param Request $request
+     * @return Response
+     */
+    public function editCVById(string $id, Request $request): Response
+    {
+        $newData = $request->toArray();
+        try {
+            Resume::validateCV($newData);
+        } catch (\Exception $exception) {
+            return $this->json([
+                'code' => 400,
+                'message' => 'Incorrect new status'
+            ], 400);
+        }
+        $client = new DB($this->getParameter('app.db_url'));
+        $collection = $client->selectCollection($this->getParameter('app.db_name'), $this->getParameter('app.db_collection'));
+        try {
+            // try to update item by id
+            $result = $collection->updateOne(['_id' => new ObjectId($id)], ['$set' => $newData]);
+            // if item has been modified
+            if ($result->getModifiedCount() === 1) {
+                return $this->json([
+                    'id' => $id,
+                    'message' => 'Resume has been successfully updated'
+                ], 200);
+            } // if item not found
+            else if ($result->getMatchedCount() === 0) {
+                return $this->json([
+                    'code' => 404,
+                    'message' => 'Resume with id ' . $id . ' not found'
+                ], 404);
+            }
+        } catch (\Exception $exception) {
+            return $this->json([
+                'code' => 400,
+                'message' => 'Incorrect resume id'
+            ], 400);
+        }
+        return $this->json([
+            'id' => $id,
+            'message' => 'Nothing changed'
+        ], 200);
     }
 
     /**
@@ -78,53 +117,41 @@ class ResumeController extends AbstractFOSRestController
     public function changeCVStatus(string $id, Request $request): Response
     {
         $newStatus = $request->toArray()['new_status'];
-        if(Resume::validateNewStatus($newStatus))
-        {
+        if (Resume::validateNewStatus($newStatus)) {
             $client = new DB($this->getParameter('app.db_url'));
             $collection = $client->selectCollection($this->getParameter('app.db_name'), $this->getParameter('app.db_collection'));
             try {
                 // try to update item by id
                 $result = $collection->updateOne(['_id' => new ObjectId($id)], ['$set' => ['resumeStatus' => $newStatus]]);
                 // if item has been modified
-                if($result->getModifiedCount() === 1)
-                {
+                if ($result->getModifiedCount() === 1) {
                     return $this->json([
                         'id' => $id,
                         'message' => 'Resume status has been successfully updated'
-                    ],
-                    200);
-                }
-                // if item not found
-                else if($result->getMatchedCount() === 0)
-                {
+                    ], 200);
+                } // if item not found
+                else if ($result->getMatchedCount() === 0) {
                     return $this->json([
                         'code' => 404,
                         'message' => 'Resume with id ' . $id . ' not found'
-                    ],
-                    404);
+                    ], 404);
                 }
-            }
-            catch (\Exception $exception)
-            {
+            } catch (\Exception $exception) {
                 return $this->json([
                     'code' => 400,
                     'message' => 'Incorrect resume id'
-                ],
-                400);
+                ], 400);
             }
-        }
-        else{
+        } else {
             return $this->json([
                 'code' => 400,
                 'message' => 'Incorrect new status'
-            ],
-            400);
+            ], 400);
         }
         return $this->json([
             'id' => $id,
             'message' => 'Nothing changed'
-        ],
-        200);
+        ], 200);
     }
 
     /**
@@ -136,8 +163,7 @@ class ResumeController extends AbstractFOSRestController
     {
         $client = new DB($this->getParameter('app.db_url'));
         $collection = $client->selectCollection($this->getParameter('app.db_name'), $this->getParameter('app.db_collection'));
-        try
-        {
+        try {
             // validate resume
             Resume::validateCV($request->toArray());
             // try to add resume
@@ -145,15 +171,13 @@ class ResumeController extends AbstractFOSRestController
             return $this->json([
                 'id' => $result->getInsertedId()->__toString()
             ],
-            201);
-        }
-        catch (\Exception $exception)
-        {
+                201);
+        } catch (\Exception $exception) {
             return $this->json([
                 'code' => 400,
                 'message' => $exception->getMessage()
             ],
-            400);
+                400);
         }
     }
 }
